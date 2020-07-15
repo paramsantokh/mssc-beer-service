8826bf75c4df23e216ac30767650f042f9907ec7
p.singh <p.singh@synchronoss.com>
2020-07-15 10:57:10 +0530

    commit #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * @author param singh
+ */
+public enum BeerStyleEnum {
+  LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
