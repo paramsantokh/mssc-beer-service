0342b890f6a1bf87c00ae111b9c6475fd9c95741
p.singh <p.singh@synchronoss.com>
2020-07-15 10:31:36 +0530

    Initial commit

---
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+}
